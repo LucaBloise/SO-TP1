baeb876b7ebe027a41f970b7631e38daff3008bb
CTepedino <ctepedino@itba.edu.ar>
2023-09-15 18:42:27 -0300

    ADT de shm

---
+++ sharedMemADT.c
@@ -0,0 +1,96 @@
+#include "sharedMemADT.h"
+
+sharedMem createSharedMem(char * name, int size){
+    sharedMem shm = malloc(sizeof(struct sharedMem));
+    shm->size = size;
+    shm->readOffset = 0;
+    shm->writeOffset = 0;
+    if ((shm->fd = shm_open(name, O_CREAT | O_RDWR,  S_IRUSR | S_IWUSR))==-1){
+        perror("Shm_open");
+        exit(EXIT_FAILURE);
+    }
+    if (ftruncate(shm->fd, shm->size)==-1){
+        perror("Ftruncate");
+        exit(EXIT_FAILURE);
+    }
+    if ((shm->startAddress = mmap(NULL, shm->size, PROT_WRITE | PROT_READ, MAP_SHARED, shm->fd, 0))==MAP_FAILED){
+        perror("Mmap");
+        exit(EXIT_FAILURE);
+    }
+    if ((shm->sem = sem_open(name, O_CREAT, S_IRUSR|S_IWUSR, 0))==SEM_FAILED){
+        perror("Sem_open");
+        exit(EXIT_FAILURE);
+    }
+    return shm;
+}
+
+sharedMem connectToSharedMem(char * name, int size){
+    sharedMem shm = malloc(sizeof(struct sharedMem));
+    shm->size = size;
+    shm->readOffset = 0;
+    shm->writeOffset = 0;
+    if ((shm->fd = shm_open(name, O_RDONLY, S_IRUSR)) == -1){
+        perror("Shm_open");
+        exit(EXIT_FAILURE);
+    }
+    if ((shm->startAddress = mmap(NULL, size, PROT_READ, MAP_SHARED, shm->fd, 0))==MAP_FAILED){
+        perror("Mmap");
+        exit(EXIT_FAILURE);
+    }
+    if ((shm->sem = sem_open(name,0))==SEM_FAILED){
+        perror("Sem_open");
+        exit(EXIT_FAILURE);
+    }
+    return shm;
+}
+
+char * readSharedMem(sharedMem shm){
+   char * read = shm->startAddress + shm->readOffset;
+   shm->readOffset += strlen(read);
+   return read;
+}
+
+void writeSharedMem(sharedMem shm, char * string, int n){
+    memcpy(shm->startAddress + shm->writeOffset, string, n);
+    shm->writeOffset += n;
+}
+
+void closeSharedMem(sharedMem shm) {
+    if (munmap(shm->startAddress, shm->size) == -1) {
+        perror("Munmap");
+        exit(EXIT_FAILURE);
+    }
+    close(shm->fd);
+    sem_close(shm->sem);
+    free(shm);
+}
+
+void unlinkSharedMem(char name[]){
+    if (sem_unlink(name)==-1){
+        perror("Sem_unlink");
+        exit(EXIT_FAILURE);
+    }
+    if (shm_unlink(name)==-1){
+        perror("Shm_unlink");
+        exit(EXIT_FAILURE);
+    }
+}
+
+void semaphoreUp(sharedMem shm){
+    if (sem_post(shm->sem)==-1){
+        perror("Sem_post");
+        exit(EXIT_FAILURE);
+    }
+}
+
+void semaphoreDown(sharedMem shm){
+    if (sem_wait(shm->sem)==-1){
+        perror("Sem_wait");
+        exit(EXIT_FAILURE);
+    }
+}
+
+
+
+
+
---
+++ sharedMemADT.h
@@ -0,0 +1,39 @@
+#ifndef SHAREDMEMADT_H
+#define SHAREDMEMADT_H
+
+#include <string.h>
+#include <sys/mman.h>
+#include <sys/stat.h>
+#include <fcntl.h>
+#include <stdlib.h>
+#include <stdio.h>
+#include <unistd.h>
+#include <sys/types.h>
+#include <semaphore.h>
+
+#define MAX_NAME_LENGTH 10
+
+struct sharedMem{
+    char name[MAX_NAME_LENGTH];
+    int fd;
+    int size;
+    void * startAddress;
+    sem_t * sem;
+    int readOffset;
+    int writeOffset;
+};
+
+typedef struct sharedMem * sharedMem;
+
+sharedMem createSharedMem(char * name, int size);
+sharedMem connectToSharedMem(char * name, int size);
+
+void closeSharedMem(sharedMem shm);
+void unlinkSharedMem(char name[]);
+
+char * readSharedMem(sharedMem shm);
+void writeSharedMem(sharedMem shm, char * string, int n);
+
+void semaphoreUp(sharedMem shm);
+void semaphoreDown(sharedMem shm);
+#endif
